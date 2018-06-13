614bc0e97b665cdd307f39991a1628b54a56584c
CalcoastWineries <35215270+CalcoastWineries@users.noreply.github.com>
2018-06-13 10:48:51 +0800

    Beautified the printing output.

--- consumer.py
+++ consumer.py
@@ -3,6 +3,8 @@ import sys
 import time
 # This script receives messages from a Kafka topic
 from kafka import KafkaConsumer
+from psycopg2.extras import RealDictCursor
+import psycopg2
 
 consumer = KafkaConsumer(
     "auth.log",
@@ -15,10 +17,20 @@ consumer = KafkaConsumer(
     ssl_keyfile="service.key",
 )
 
-print ("Hello World!")
+#uri = "postgres://avnadmin:m3382yo5hlz9aopp@pg-19209ef7-freddyso-c424.aivencloud.com:11141/kafka-demo?sslmode=require"
+uri = sys.argv[2]
 
-raw_msgs = consumer.poll(timeout_ms=1000)
-for tp, msgs in raw_msgs.items():
+db_conn = psycopg2.connect(uri)
+c = db_conn.cursor(cursor_factory=RealDictCursor)
+
+print ("Hello World! I am Kafka Consumer.")
+
+while True:
+  raw_msgs = consumer.poll(timeout_ms=1000)
+  for tp, msgs in raw_msgs.items():
     for msg in msgs:
-        print("Received: {}".format(msg.value))
+        print("I Received: {}".format(msg.value))
+	c.execute("insert into authlog_messages (kafka_consumer, message) values ('demo_consumer','%s')" % (format(msg.value))) 
+	db_conn.commit()
+    print("I just committed the last few messages into PostgreSQL.")
 
--- producer.py
+++ producer.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
+import sys
 import time
 # This script connects to Kafka and send a few messages 
 from kafka import KafkaProducer
 
 producer = KafkaProducer(
-    bootstrap_servers="kafka-32fe272c-freddyso-c424.aivencloud.com:11143",
+    bootstrap_servers=sys.argv[1],
     security_protocol="SSL",
     ssl_cafile="ca.pem",
     ssl_certfile="service.cert",
     ssl_keyfile="service.key",
 )
 
-print ("Hello World!")
+print ("Hello World!  I am Kafka Producer.")
 
 def follow(thefile):
   thefile.seek(0,2)
@@ -26,10 +27,10 @@ if __name__ == '__main__':
   logfile = open('/var/log/auth.log', 'r')
   loglines = follow(logfile)
   for line in loglines:
-    print ('do something here')
-    print (line)
+#    print ('do something here')
+#    print (line)
 
-    print("Sending: {}".format(line))
+    print("I am Sending: {}".format(line))
     producer.send("auth.log", line.encode("utf-8"))
 
 # Wait for all messages to be sent
